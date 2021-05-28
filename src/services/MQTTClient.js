05299c457a53b32b21be7976715b82ff716201ef
CesarRawr <rawrcesar13@gmail.com>
2021-05-27 23:27:21 -0500

    new if added

--- src/services/MQTTClient.js
+++ src/services/MQTTClient.js
@@ -7,7 +7,9 @@ const client = mqtt.connect('mqtt://broker.emqx.io', {
 
 client.on('message', (topic, message, packet) => {
 	const note = JSON.parse(message);
-	Notes.addNote(note);
+	if (!!(note.type) === false) {
+		Notes.addNote(note);
+	}
 });
 
 client.on("error", (error) => {
